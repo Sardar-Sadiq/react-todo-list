31b62f1476115111fa16f0bb856129f76e2d5ead
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-08-26 02:17:13 +0530

    backend notes

--- Backend/server.js
+++ Backend/server.js
@@ -4,10 +4,11 @@ const express = require("express")
 
 const app = express();
 
-
 app.get("/api/notes", (req,res) => {
-    res.send("you got 5 notes");
+   // send the notes
+    res.status(200).send("you got 5 notes");
 })
+
 
 app.listen(5001, ()=> {
     console.log("Server started on PORT 5001");
