3c3f7b987a07cb83cefbed51f6f19ae8815b8504
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-08-25 23:10:15 +0530

    created backend server starting steps

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,11 @@ const express = require("express")
 
 const app = express();
 
+
+app.get("/api/notes", (req,res) => {
+    res.send("you got 5 notes");
+})
+
 app.listen(5001, ()=> {
     console.log("Server started on PORT 5001");
 })
