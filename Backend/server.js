83a7d0dbf1c2816a31803fea5de5fd9066292829
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-09-08 01:38:48 +0530

    working on backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,13 @@ const app = express();
 
 app.get("/api/notes", (req,res) => {
    // send the notes
-    res.status(200).send("you got 20 notes");
+    res.status(200).send("you got 50 notes");
 })
 
+app.post("/api/notes", (req,res) => {
+    // add the notes
+     res.status(201).json("note added successfully");
+})
 
 app.listen(5001, ()=> {
     console.log("Server started on PORT 5001");
